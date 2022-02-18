a7f86aff925a6eb01aac4ca0c87da63b35164c0b
MOSQUEDACORDOVA <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2022-02-18 10:51:50 -0500

    consulta

--- vuexy-html-bootstrap5-admin-template/html/ltr/vertical-menu-template-bordered/table-bootstrap.php
+++ vuexy-html-bootstrap5-admin-template/html/ltr/vertical-menu-template-bordered/table-bootstrap.php
@@ -841,6 +841,28 @@
                                     var_dump($response);
                                 }
 
+                                //datos a enviar
+                                $data = array("tenant" => "golfsantodomingo");
+                                //url contra la que atacamos
+                                $ch = curl_init("https://mt-america.golfmanager.com/api/clientsFull");
+                                //a true, obtendremos una respuesta de la url, en otro caso, 
+                                //true si es correcto, false si no lo es
+                                curl_setopt($ch, CURLOPT_USERPWD, "yPySDyHjC7".":"."bVBMuj4y3JZmdu6"); 
+                                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                                //establecemos el verbo http que queremos utilizar para la petición
+                                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                                //enviamos el array data
+                                curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
+                                //obtenemos la respuesta
+                                $response = curl_exec($ch);
+                                // Se cierra el recurso CURL y se liberan los recursos del sistema
+                                curl_close($ch);
+                                if(!$response) {
+                                    return false;
+                                }else{
+                                    return $response;
+                                }
+
                             ?>
                                 <table class="table">
                                     <thead>
